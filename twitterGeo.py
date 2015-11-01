690a57d1dc2e8e0048eb9dee4286a3bde6274c0a
U-TIEN-PC\Tien Tran <ttran088@ucr.edu>
2015-11-01 15:00:40 -0800

    .

--- twitterGeo.py
+++ twitterGeo.py
@@ -9,7 +9,7 @@ access_token_secret = "zfR4t6WM2Zmf5185uW3aJ6xxqnth8lwZYMoBNtvsPypDR"
 consumer_key = "8uzP5HaulOr2a5z9WUOiegkqf"
 consumer_secret = "DpfkvmXmcy23PReWBVZEUziFRSjo9ZxClMGY6MIpiTmtajl8cS"
 
-f = open('data.txt', 'w')
+f = open('data/twitter_data.txt', 'w')
 
 
 #twitter listener
@@ -18,25 +18,27 @@ class twitterListener(StreamListener):
     def on_data(self, data):
         decoded = json.loads(data)
 
-        if decoded.has_key('user'):
-            username = unicode(decoded['user']['screen_name']) #gets username
-            userTweet = unicode(decoded['text'].encode('ascii', 'ignore')) #gets tweet
-            userTweetTime = unicode(decoded['created_at']) #gets timestamp
-            userLocation = unicode(decoded['user']['location']) #gets location
-            userCoords = unicode(decoded['coordinates']) #gets coordinates
 
-            userData = userTweetTime + " @" + username + ": " + userTweet + "\n"
-            print userData
-            f.write(userData)
+        username = unicode(decoded['user']['screen_name']) #gets username
+        userTweet = unicode(decoded['text'].encode('ascii', 'ignore')) #gets tweet
+        userTweetTime = unicode(decoded['created_at']) #gets timestamp
+        userLocation = unicode(decoded['user']['location']) #gets location
+        userCoords = unicode(decoded['coordinates']) #gets coordinates
+        userHashtags = unicode(decoded['entities']['hashtags'])
+        '''
+        for Hashtags in userHashtags:
+            userHashtags = Hashtags['text']
+            print decoded['text'] + str(userHashtags)
+        '''
+        userData = userTweetTime + " @" + username + ": " + userTweet
+        print userData
+        f.write(userData)
 
         
-        '''       
-        userHashtags = unicode(decoded['entities']['hashtags'])
-        userURLS = unicode(decoded['entities']['urls'])
-        print userHashtags
-        print userURLS
-        '''
-        print "\n"
+       # userURLS = unicode(decoded['entities']['urls'])
+        #print userHashtags
+       # print userURLS
+        
         return True
 
     def on_error(self, status):
@@ -50,8 +52,6 @@ if __name__ == '__main__':
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     stream = Stream(auth, l)
-
-    tweets_data_path = '../data/twitter_data.txt'
 
     stream = Stream(auth, l)
     stream.filter(locations=[-122.75,36.8,-121.75,37.8]) #filter tweets to be in the San Francisco area
