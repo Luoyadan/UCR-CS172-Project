b645ab5ff873feca3ca8c2a4f3b124b68778cc6f
U-TIEN-PC\Tien Tran <ttran088@ucr.edu>
2015-11-01 14:31:15 -0800

    now writes to a file called data.txt

--- twitterGeo.py
+++ twitterGeo.py
@@ -9,6 +9,8 @@ access_token_secret = "zfR4t6WM2Zmf5185uW3aJ6xxqnth8lwZYMoBNtvsPypDR"
 consumer_key = "8uzP5HaulOr2a5z9WUOiegkqf"
 consumer_secret = "DpfkvmXmcy23PReWBVZEUziFRSjo9ZxClMGY6MIpiTmtajl8cS"
 
+f = open('data.txt', 'w')
+
 
 #twitter listener
 class twitterListener(StreamListener):
@@ -23,8 +25,9 @@ class twitterListener(StreamListener):
             userLocation = unicode(decoded['user']['location']) #gets location
             userCoords = unicode(decoded['coordinates']) #gets coordinates
 
-            userData = userTweetTime + " @" + username + ": " + userTweet
+            userData = userTweetTime + " @" + username + ": " + userTweet + "\n"
             print userData
+            f.write(userData)
 
         
         '''       
@@ -52,3 +55,4 @@ if __name__ == '__main__':
 
     stream = Stream(auth, l)
     stream.filter(locations=[-122.75,36.8,-121.75,37.8]) #filter tweets to be in the San Francisco area
+    f.close()
