e7a153272657bea693ffdd8e70c940aa94db0be5
khuan013 <khuan013@ucr.edu>
2015-11-02 21:55:03 -0800

    Added an executable file for Unix/Linux

---
+++ crawler.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+mkdir -p data
+FILEPATH="data"
+else
+mkdir -p $1
+FILEPATH=$1
+fi
+
+echo "Writing twitter data to /$FILEPATH ..."
+python twitterGeo.py $FILEPATH
