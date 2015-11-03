d9817b47bbb7a2652aaff1dcb31419fcbd96584e
U-TIEN-PC\Tien Tran <ttran088@ucr.edu>
2015-11-03 10:24:28 -0800

    changed ordering of parameter

--- crawler.sh
+++ crawler.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-if [ -z "$1" ]; then
+if [ -z "$2" ]; then
 mkdir -p data
 FILEPATH="data"
 else
-mkdir -p $1
-FILEPATH=$1
+mkdir -p $2
+FILEPATH=$2
 fi
 
 echo "Writing twitter data to /$FILEPATH ..." 
-python twitterGeo.py $FILEPATH $2
+python twitterGeo.py $1 $FILEPATH 
